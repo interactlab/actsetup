6a59bc36e41b884aa61b97e63cbaa36aa3ea2976
Nick Landolfi <nclandolfi@gmail.com>
2016-09-30 12:47:00 -0700

    add auto -y

--- install_ros.sh
+++ install_ros.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-set -e 
+set -e
 
 echo " * Adding ROS package server to apt sources"
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 echo " * Grabbing ROS key to ensure not malicious packages"
 sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net --recv-key 0xB01FA116
 echo " * Ensuring up-to-date package index"
-sudo apt-get update
+sudo apt-get -y update
 echo " * Installing ros indigo desktop full"
-sudo apt-get install ros-indigo-desktop-full
+sudo apt-get -y install ros-indigo-desktop-full
 echo " ==> Installed, to find packages: `apt-cache search ros-indigo`"
 echo " * Intializing `rosdep` (`sudo rosdep init`)"
 if [ -e /etc/ros/rosdep/sources.list.d/20-default.list ]; then
