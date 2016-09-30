57aaa8e30d00cad4d105b2375121e2c1f1bb3f13
Nick Landolfi <nclandolfi@gmail.com>
2016-09-30 14:33:23 -0700

    add install.sh that does everything

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -e
+
+bash <(curl -s http://mywebsite.com/myscript.tx://raw.githubusercontent.com/nlandolfi/actsetup/master/install_ros.sh)
+bash <(curl -s http://mywebsite.com/myscript.tx://raw.githubusercontent.com/nlandolfi/actsetup/master/install_openrave.sh)
+bash <(curl -s http://mywebsite.com/myscript.tx://raw.githubusercontent.com/nlandolfi/actsetup/master/install_act.sh)
