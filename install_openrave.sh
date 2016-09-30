216a1062ed7a0a334399385cd98337b3d758a91a
Ubuntu <ubuntu@ip-172-31-4-213.us-west-2.compute.internal>
2016-09-30 19:33:08 +0000

    add bash scripts to install ros and openrave

---
+++ install_openrave.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+set -e
+
+#echo " * Adding apt repo for openrave"
+#sudo add-apt-repository ppa:openrave/release
+#echo " ==> Added"
+#echo " * sudo apt-get install openrave"
+#sudo apt-get install openrave
+#echo " ==> Installed"
+
+echo " * sudo apt-get update"
+sudo apt-get update
+echo " ==> Updated"
+
+echo " * Installing lots of build dependencies"
+sudo apt-get install libbz2-dev libpcre3-dev libcoin80-dev libsoqt4-dev libode-dev libann-dev libqhull-dev libassimp-dev liboctave-dev libavcodec-dev libavformat-dev libswscale-dev python-dev python-numpy python-sympy libboost1.54-dev libboost-date-time1.54-dev libboost-filesystem1.54-dev libboost-iostreams1.54-dev libboost-python1.54-dev libboost-regex1.54-dev libboost-system1.54-dev libboost-thread1.54-dev libxml2-dev libopenscenegraph-dev python-numpy python-scipy python-matplotlib
+echo " ==> Installed"
+
+
+echo " * Cloning openrave project"
+if [ -e openrave ]; then
+	sudo rm -rf openrave
+fi
+git clone https://github.com/personalrobotics/openrave
+echo " ==> Cloned"
+
+cd openrave
+mkdir build
+cd build
+echo " * Building OpenRAVE"
+cmake -DCMAKE_INSTALL_PREFIX=/usr .. > outputcmake.txt
+sudo make install
+echo " ==> Built"
+
+if [ -e ~/catkin_ws/ ]; then
+	read -p "Are you sure? " -n 1 -r
+	echo    # (optional) move to a new line
+	if [[ ! $REPLY =~ ^[Yy]$ ]]
+	then
+	    exit 1
+	fi
+	rm -rf ~/catkin_ws/
+	mkdir -p ~/catkin_ws/src
+else
+	mkdir -p ~/catkin_ws/src
+fi
+
+cd ~/catkin_ws/src
+catkin_init_workspace
+cd ~/catkin_ws/
+catkin_make
---
+++ install_ros.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -e 
+
+echo " * Adding ROS package server to apt sources"
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+echo " * Grabbing ROS key to ensure not malicious packages"
+sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net --recv-key 0xB01FA116
+echo " * Ensuring up-to-date package index"
+sudo apt-get update
+echo " * Installing ros indigo desktop full"
+sudo apt-get install ros-indigo-desktop-full
+echo " ==> Installed, to find packages: `apt-cache search ros-indigo`"
+echo " * Intializing `rosdep` (`sudo rosdep init`)"
+if [ -e /etc/ros/rosdep/sources.list.d/20-default.list ]; then
+	echo " ** Found sources list already present, removing"
+	sudo rm /etc/ros/rosdep/sources.list.d/20-default.list
+fi
+sudo rosdep init
+echo " ==> Initialized"
+echo " * Updating rosdep (`rosdep update`)"
+rosdep update
+echo " ==> Updated"
+echo " * Adding ROS setup.sh call to bashrc"
+echo "source /opt/ros/indigo/setup.bash" >> ~/.bashrc
+source ~/.bashrc
+echo " * Installing rosinstall"
+sudo apt-get install python-rosinstall
+echo " ==> Installed"
