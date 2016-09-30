9103c86e97e9a59c32e961bc935c1b6c86a57284
Nick Landolfi <nclandolfi@gmail.com>
2016-09-30 15:41:00 -0700

    fix catkin_make order

--- install_act.sh
+++ install_act.sh
@@ -10,15 +10,15 @@ git submodule update --init --recursive
 
 source ~/.bashrc # just in case for catkin_init_workspace
 catkin_init_workspace
-echo " * Adding catkin devel/setup.sh call to bashrc"
-echo "source ~/catkin_ws/devel/setup.sh" >> ~/.bashrc
-source ~/.bashrc
-
 cd ..
-source ./devel/setup.sh
 catkin_make
 
 git config --global core.editor vim # :p
 sudo apt-get -y install python-pip
 sudo pip uninstall sympy
 sudo pip install sympy==0.7.1
+source ./devel/setup.sh
+echo " * Adding catkin devel/setup.sh call to bashrc"
+echo "source ~/catkin_ws/devel/setup.sh" >> ~/.bashrc
+source ~/.bashrc
+
