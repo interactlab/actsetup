968dea32247e94fcc85210ba0273866cf96d7a04
Nick Landolfi <nclandolfi@gmail.com>
2016-11-07 15:58:43 -0800

    clean up/comments

--- install_act.sh
+++ install_act.sh
@@ -2,10 +2,14 @@
 
 set -e
 
-sudo apt-get -y install libtinyxml2-dev # ensure tinyxml is installed
-sudo apt-get -y install ros-indigo-srdfdom
+sudo apt-get -y install \
+    libtinyxml2-dev="0~git20120518.1.a2ae54e-1" \ # ensure tinyxml is installed
+    ros-indigo-srdfdom="0.3.3-0trusty-20160909-191729-0700" \
+    python-pip="1.5.4-1ubuntu4"
 cd ~/catkin_ws/src
-git clone https://github.com/nlandolfi/actsrc.git ./
+git clone https://github.com/interactlab/actsrc.git ./
+# We have our ros dependencies as git modules, therefore
+# we need to grab download all of these
 git submodule update --init --recursive
 
 source ~/.bashrc # just in case for catkin_init_workspace
@@ -14,11 +18,14 @@ cd ..
 catkin_make
 
 git config --global core.editor vim # :p
-sudo apt-get -y install python-pip
+
+# new sympy version causes a bug (?) in ikfast, so revert to known
+# working old version
 sudo pip uninstall sympy
 sudo pip install sympy==0.7.1
+
 source ./devel/setup.sh
 echo " * Adding catkin devel/setup.sh call to bashrc"
 echo "source ~/catkin_ws/devel/setup.sh" >> ~/.bashrc
 source ~/.bashrc
-
+echco "=> Added"
